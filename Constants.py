4fa004bdaabde802993bff221f1605fe72ca9162
Radmila Kompova <radmilko@ifi.uio.no>
2020-05-08 16:33:21 +0200

    refactor submitJob, add scz-related code, create constants file

---
+++ Constants.py
@@ -0,0 +1,6 @@
+
+YAML_FILENAME = "config.yml"
+#BASEPATH = "/home/ubuntu/imputeDisk/01-workspace/00-temp/bifrost-testing/"
+BASEPATH = "/home/centos/bifrost-test"
+#REMOTE_PRIVATE_KEY_PATH = '/home/ubuntu/.c4gh/nrec.sec'
+REMOTE_PRIVATE_KEY_PATH = '/home/centos/bifrost-remote-test.sec'
